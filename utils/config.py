eab3774fe6e69c418651835c742f7f6d8185bd78
datn2107 <65413565+datn2107@users.noreply.github.com>
2021-09-22 00:05:57 +0700

    fix bug

--- utils/config.py
+++ utils/config.py
@@ -25,15 +25,15 @@ def _get_true_type_from_str(value: str) -> Any:
 def _get_section(config_parser: configparser, section: str) -> Dict:
     section_value = dict(config_parser[section])
     for key, value in section_value.items():
-        section_value[key] = get_true_type_from_str(value)
+        section_value[key] = _get_true_type_from_str(value)
     return section_value
 
 def get_path_from_config(config_path: str) -> Dict:
-    path_config = get_section(read_config(config_path), 'Path')
+    path_config = _get_section(_read_config(config_path), 'Path')
     return path_config
 
 def get_data_from_config(config_path: str) -> Dict:
-    data_config = get_section(read_config(config_path), 'Data')
+    data_config = _get_section(_read_config(config_path), 'Data')
     if 'batch_size' not in data_config:
         data_config['batch_size'] = 8
     if 'epoch' not in data_config:
@@ -44,11 +44,11 @@ def get_data_from_config(config_path: str) -> Dict:
 
 def get_model_from_config(config_path: str) -> Dict:
     # Get config of base model
-    model_config = get_section(read_config(config_path), 'Base Model')
+    model_config = _get_section(_read_config(config_path), 'Base Model')
     if 'model_name' not in model_config:
         model_config['model_name'] = 'ResNet50V2'
     # Get config of fully connected layer
-    fully_connected_layer_config = get_section('Fully Connected Layer')
+    fully_connected_layer_config = _get_section('Fully Connected Layer')
     if fully_connected_layer_config['num_dense'] <= 0:
         print("Number dense layer is less than 1. It will set to the default num_dense = 3")
         fully_connected_layer_config['num_dense'] = 3
@@ -56,20 +56,20 @@ def get_model_from_config(config_path: str) -> Dict:
     return {**model_config, **fully_connected_layer_config}
 
 def get_optimizer_from_config(config_path: str) -> Dict:
-    optimizer_config = get_section(read_config(config_path), 'Optimizer')
+    optimizer_config = _get_section(_read_config(config_path), 'Optimizer')
     return optimizer_config
 
 def get_loss_from_config(config_path: str) -> Dict:
-    loss_config = get_section(read_config(config_path), 'Loss')
+    loss_config = _get_section(_read_config(config_path), 'Loss')
     return loss_config
 
 def get_list_metric_from_config(config_path: str) -> List[Dict]:
     list_metric_config = []
 
-    config_parser = read_config(config_path)
+    config_parser = _read_config(config_path)
     for section in config_parser.sections():
         if "Metric" not in section:
             continue
-        metric_config = get_section(config_parser, section)
+        metric_config = _get_section(config_parser, section)
         list_metric_config.append(metric_config)
     return list_metric_config
