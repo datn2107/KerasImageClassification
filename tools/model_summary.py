2c02dc26f1682e6a4917460c79d7430dde662d16
datn2107 <65413565+datn2107@users.noreply.github.com>
2021-09-22 00:42:38 +0700

    fix bug

--- tools/model_summary.py
+++ tools/model_summary.py
@@ -8,7 +8,7 @@ if __name__ == '__main__':
     package_dir = os.path.dirname(os.path.dirname(os.path.realpath(__file__)))
     parser = argparse.ArgumentParser()
     parser.add_argument('--config', type=str, help='Config Path')
-    parser.set_defaults(config=os.path.join(package_dir, "config/model.cfg"))
+    parser.set_defaults(config=os.path.join(package_dir, "configs", "model.cfg"))
 
     model_config = get_model_from_config(parser.parse_args().config)
     model_generator = KerasModel(**model_config, num_class=10)
