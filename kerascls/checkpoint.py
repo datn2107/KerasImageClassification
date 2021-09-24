895e272534d5d8948dec5d682ff6ee7e3361c2e5
datn2107 <65413565+datn2107@users.noreply.github.com>
2021-09-24 18:27:38 +0700

    Fix bug

--- kerascls/checkpoint.py
+++ kerascls/checkpoint.py
@@ -4,18 +4,6 @@ import warnings
 import tensorflow as tf
 
 
-def load_latest_weight(weights_cp_dir=None):
-    latest_checkpoint = None
-    for checkpoint in os.listdir(weights_cp_dir):
-        checkpoint = os.path.join(weights_cp_dir, checkpoint)
-        if ".index" in checkpoint:
-            if latest_checkpoint is None:
-                latest_checkpoint = checkpoint
-            elif os.path.getmtime(latest_checkpoint) < os.path.getmtime(checkpoint):
-                latest_checkpoint = checkpoint
-    return ".".join(latest_checkpoint.split('.')[:-1])
-
-
 def load_model(model, model_cp_dir=None, hdf5_cp_path=None, **ignore):
     if model_cp_dir is not None:
         model = tf.keras.models.load_model(model_cp_dir)
@@ -28,12 +16,24 @@ def load_model(model, model_cp_dir=None, hdf5_cp_path=None, **ignore):
     return model
 
 
+def load_latest_weight(weights_cp_dir=None):
+    latest_checkpoint = None
+    for checkpoint in os.listdir(weights_cp_dir):
+        checkpoint = os.path.join(weights_cp_dir, checkpoint)
+        if ".index" in checkpoint:
+            if latest_checkpoint is None:
+                latest_checkpoint = checkpoint
+            elif os.path.getmtime(latest_checkpoint) < os.path.getmtime(checkpoint):
+                latest_checkpoint = checkpoint
+    return ".".join(latest_checkpoint.split('.')[:-1])
+
+
 def load_weight(model, weights_cp_dir=None, weights_cp_path=None, **ignore):
     if weights_cp_path is not None or weights_cp_dir is not None:
         if weights_cp_dir is not None:
             weights_cp_path = load_latest_weight(weights_cp_dir=weights_cp_dir)
-        model = model.load_weights(weights_cp_path)
-        print("Load checkpoints from ", weights_cp_path, ".")
+        model.load_weights(weights_cp_path)
+        print("Load checkpoints from ", weights_cp_path)
     else:
         warnings.warn("There are no weights checkpoint to load.")
     return model
