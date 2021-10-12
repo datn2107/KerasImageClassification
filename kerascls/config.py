dad2c6e2895b60f4a6467a8b9ff41517d5a7e4ea
datn2107 <65413565+datn2107@users.noreply.github.com>
2021-10-12 11:59:28 +0700

    Add comment and doc

--- kerascls/config.py
+++ kerascls/config.py
@@ -1,15 +1,38 @@
 import configparser
+import os
 import warnings
 from typing import List, Dict, Any
 
 
 class ConfigReader:
     def __init__(self, config_path: str):
+        """Read data from config and return to dict for each section
+
+        Allowed section:
+            Path: Contain path to image directory, metadata and saving directory
+            Data: Contain the fraction of each part of data (training, validation, testing)
+            Checkpoint: Contain path to checkpoint
+            Input Shape: Contain size of the image size to resize
+            Base Model: Contain the argument of the base model
+            Fully Connected Layer: Contain the config for fully connected layer
+            Optimizer: Contain name of optimizer and arguments for its
+            Loss: Contain name of loss and arguments for its
+            Metric*: * is mean you can create more than 1 section with prefix string is "Metric"
+                     Each section of this will contain name of metric and its arguments
+
+        Using assert instead of raise to show raise error because assert will show specific error
+
+        :param config_path: Absolute path to config file
+        """
+
         self.config_parser = configparser.ConfigParser(allow_no_value=True)
         self.config_parser.read(config_path)
 
     @classmethod
-    def get_true_type_from_str(cls, value: str) -> Any:
+    def _get_true_type_from_str(cls, value: str) -> Any:
+        """Convert str to truth data type"""
+
+        # Because all data read from config is str
         if value == "None":
             return None
         elif value == "True" or value == "False":
@@ -22,60 +45,105 @@ class ConfigReader:
             except ValueError:
                 return value
 
+    @classmethod
+    def _fill_missing_key(cls, config_dict: Dict, allow_keys: List[str]) -> Dict:
+        """Assign None to the missing arguments"""
+
+        # Guarantee there are no missing arguments
+        for key in allow_keys:
+            if key not in config_dict:
+                config_dict[key] = None
+        return config_dict
+
     def get_section(self, section: str) -> Dict:
+        """Get all information in section, convert to truth datatype and store it to dict"""
+
         section_value = dict(self.config_parser[section])
         for key, value in section_value.items():
-            section_value[key] = self.get_true_type_from_str(value)
+            section_value[key] = self._get_true_type_from_str(value)
         return section_value
 
-    def get_path(self) -> Dict:
+    def get_path_config(self) -> Dict:
         path_config = self.get_section('Path')
-        if path_config.get('image_dir', None) is None:
-            raise KeyError("Missing directory path of image.")
-        if path_config.get('metadata_path', None) is None:
-            raise KeyError("Missing metadata path.")
+        assert path_config.get('image_dir', None) is None, "Missing directory path of image."
+        assert path_config.get('metadata_path', None) is None, "Missing metadata path."
+
+        # Set a default for saving directory
+        # Default of saving direction is inside main package
         if path_config.get('saving_dir', None) is None:
-            raise KeyError("Missing saving directory path to save model.")
+            path_config['saving_dir'] = os.path.join(os.path.dirname(os.path.realpath(__file__)), "saving_dir")
+            warnings.warn(f"Saving directory is missing. It will save in %s" % path_config['saving_dir'])
         return path_config
 
-    def get_checkpoint(self) -> Dict:
+    def get_data_config(self) -> Dict:
+        data_config = self.get_section('Data')
+        keys = ['train_size', 'val_size', 'test_size']
+        assert any(data_config.get(key, None) is None for key in keys), 'Missing data size arguments'
+        assert sum(data_config.values()) != 1., 'Sum of train, val and test data fraction is not equal 1'
+        return data_config
+
+    def get_checkpoint_config(self) -> Dict:
         checkpoint_config = self.get_section('Checkpoints')
+        # Set None value instead of missing arguments
+        allow_keys = ['model_cp_dir', 'hdf5_cp_path', 'weights_cp_path', 'weights_cp_dir']
+        checkpoint_config = self._fill_missing_key(checkpoint_config, allow_keys)
         if 'last_epoch' not in checkpoint_config:
             checkpoint_config['last_epoch'] = 0
         return checkpoint_config
 
-    def get_model(self) -> Dict:
-        # Get config of input shape
+    def get_model_config(self) -> Dict:
         input_config = self.get_section('Input Shape')
-        input_config = {'input_shape': (input_config['height'], input_config['width'], 3)}
-        if input_config['input_shape'] == (None, None, 3):
-            input_config['input_shape'] = (224, 224, 3)
-        # Get config of base model
+        input_config = {'input_shape': (input_config.get('height', None), input_config.get('width', None), 3)}
         model_config = self.get_section('Base Model')
-        if 'model_name' not in model_config:
-            model_config['model_name'] = 'ResNet50V2'
-        # Get config of fully connected layer
         fully_connected_layer_config = self.get_section('Fully Connected Layer')
-        if fully_connected_layer_config['num_dense'] <= 0:
-            warnings.warn("Number dense layer is less than 1. It will set to the default num_dense = 3")
-            fully_connected_layer_config['num_dense'] = 3
-        # combine two dictionary in one
-        return {**input_config, **model_config, **fully_connected_layer_config}
 
-    def get_optimizer(self) -> Dict:
+        full_model_config = {**input_config, **model_config, **fully_connected_layer_config}
+
+        # Only need to guarantee the model_name and num_dense are not missing or not None
+        # because other arguments is has default value in build model class
+        if full_model_config.get('model_name', None) is None:
+            full_model_config['model_name'] = 'ResNet50V2'
+        # guarantee that num_dense not less than 1
+        if full_model_config.get('num_dense', None) is not None \
+                and full_model_config.get('num_dense') <= 0:
+            warnings.warn("Number dense layer is less than 1. It will set to the default num_dense = 1")
+            full_model_config['num_dense'] = 1
+
+        # Ignore parameter instead of None value in base_model and fully_connected_layer section
+        # because this will pass to function by **dict
+        # and parameters in function is already has default value
+        # so if it has None value it will broke default value of function
+        if full_model_config['input_shape'][0] is None or full_model_config['input_shape'][1] is None:
+            full_model_config.pop('input_shape')
+        keys = ['backbone_weights', 'trainable_backbone', 'last_pooling_layer',
+                'num_dense', 'unit_first_dense_layer', 'units_remain_rate',
+                'activation_dense', 'activation_last_dense', 'dropout_layer', 'dropout_rate']
+        for key in keys:
+            if key in full_model_config and full_model_config.get(key) is None:
+                full_model_config.pop(key)
+
+        return full_model_config
+
+    def get_optimizer_config(self) -> Dict:
         optimizer_config = self.get_section('Optimizer')
+        if 'optimizer' not in optimizer_config:
+            optimizer_config['optimizer'] = 'SGD'
         return optimizer_config
 
-    def get_loss(self) -> Dict:
+    def get_loss_config(self) -> Dict:
         loss_config = self.get_section('Loss')
+        if 'loss' not in loss_config:
+            loss_config['loss'] = 'BinaryCrossentropy'
         return loss_config
 
-    def get_list_metric(self) -> List[Dict]:
+    def get_list_metric_config(self) -> List[Dict]:
         list_metric_config = []
 
         for section in self.config_parser.sections():
             if "Metric" not in section:
                 continue
             metric_config = self.get_section(section)
+            if 'metric' not in metric_config:
+                metric_config = 'BinaryAccuracy'
             list_metric_config.append(metric_config)
         return list_metric_config
--- kerascls/model.py
+++ kerascls/model.py
@@ -1,32 +1,35 @@
+import os
 import importlib
+import warnings
 from math import floor, ceil, log2
-from typing import Tuple, List
+from typing import Tuple
 
 import tensorflow as tf
 from tensorflow.keras.layers import Flatten, Dense, Dropout
 
-module_name = "tensorflow.keras.applications.{model_name}"
-general_model_name = {"Xception": "xception",
-                      "VGG16": "vgg16", "VGG19": "vgg19",
-                      "ResNet50": "resnet", "ResNet101": "resnet", "ResNet152": "resnet",
-                      "ResNet50V2": "resnet_v2", "ResNet101V2": "resnet_v2", "ResNet152V2": "resnet_v2",
-                      "InceptionV3": "inception_v3", "InceptionResNetV2": "inception_resnet_v2",
-                      "MobileNet": "mobilenet", "MobileNetV2": "mobilenet_v2",
-                      "DenseNet121": "densenet", "DenseNet169": "densenet", "DenseNet201": "densenet",
-                      "NASNetMobile": "nasnet", "NASNetLarge": "nasnet",
-                      "EfficientNetB0": "efficientnet", "EfficientNetB1": "efficientnet",
-                      "EfficientNetB2": "efficientnet", "EfficientNetB3": "efficientnet",
-                      "EfficientNetB4": "efficientnet", "EfficientNetB5": "efficientnet",
-                      "EfficientNetB6": "efficientnet", "EfficientNetB7": "efficientnet", }
+# These constant use to define model of tf.keras.applications when we only have model name at string type
+MODULE_NAME_FORMAT = "tensorflow.keras.applications.{model_name}"
+MODULE_NAME = {"Xception": "xception",
+               "VGG16": "vgg16", "VGG19": "vgg19",
+               "ResNet50": "resnet", "ResNet101": "resnet", "ResNet152": "resnet",
+               "ResNet50V2": "resnet_v2", "ResNet101V2": "resnet_v2", "ResNet152V2": "resnet_v2",
+               "InceptionV3": "inception_v3", "InceptionResNetV2": "inception_resnet_v2",
+               "MobileNet": "mobilenet", "MobileNetV2": "mobilenet_v2",
+               "DenseNet121": "densenet", "DenseNet169": "densenet", "DenseNet201": "densenet",
+               "NASNetMobile": "nasnet", "NASNetLarge": "nasnet",
+               "EfficientNetB0": "efficientnet", "EfficientNetB1": "efficientnet",
+               "EfficientNetB2": "efficientnet", "EfficientNetB3": "efficientnet",
+               "EfficientNetB4": "efficientnet", "EfficientNetB5": "efficientnet",
+               "EfficientNetB6": "efficientnet", "EfficientNetB7": "efficientnet", }
 
 
 class KerasModel:
-    def __init__(self, model_name: str, num_class: int, input_shape: Tuple[int, ...],
+    def __init__(self, model_name: str, num_class: int, input_shape: Tuple[int, int, int] = (224, 224, 3),
                  backbone_weights: str = "imagenet", trainable_backbone: bool = True, last_pooling_layer: str = "avg",
-                 num_dense: int = 0, unit_first_dense_layer: int = 4096, activation_dense: str = 'relu',
+                 num_dense: int = 1, unit_first_dense_layer: int = 4096, activation_dense: str = 'relu',
                  units_remain_rate: float = 0.5, activation_last_dense: str = 'sigmoid', dropout_layer: bool = True,
                  dropout_rate: float = 0.3, **ignore):
-        self.model_name = model_name
+        self.full_model = None
         self.num_class = num_class
         self.input_shape = input_shape
         self.last_pooling_layer = last_pooling_layer
@@ -38,33 +41,24 @@ class KerasModel:
         self.dropout_layer = dropout_layer
         self.dropout_rate = dropout_rate
 
-        # Load base model and their related function
-        module = importlib.import_module(module_name.format(model_name=general_model_name[self.model_name]))
-        self.base_model = getattr(module, self.model_name)(include_top=False, weights=backbone_weights,
-                                                           pooling=last_pooling_layer)
-        self.base_model.trainable = trainable_backbone
+        # Each specific model is a function of module in tf.keras.applications
+        # to load model we need to load module that contain it
+        module = importlib.import_module(MODULE_NAME_FORMAT.format(model_name=MODULE_NAME[model_name]))
+        self.backbone = getattr(module, model_name)(include_top=False, weights=backbone_weights,
+                                                    pooling=last_pooling_layer)
+        self.backbone.trainable = trainable_backbone
+        # Each model in tf.keras.applications will have different requirement about image input
+        # Some need to normalization some not, so we need to use their preprocess_input function to handle it
         self.preprocess_layer = getattr(module, "preprocess_input")
-        self.decode_predictions = getattr(module, "decode_predictions")
 
-    def _get_base_model_input_shape(self) -> Tuple[int, int, int]:
-        model = self.base_model
-
-        shape = model.input_shape
-        height, width, channel = shape[1], shape[2], shape[3]
-
-        if height is None and width is None:
-            height, width, channel = self.input_shape
-        return height, width, channel
-
-    def _get_base_model_output_shape(self) -> List:
-        model = self.base_model
-        return model.output_shape[1:]
-
-    def _create_fully_connected_layer(self, backbone) -> tf.keras.layers.Layer:
+    def _create_fully_connected_layers(self, backbone) -> tf.keras.layers.Layer:
+        # Guarantee the unit of first dense layer fit with backbone output
         self.unit_first_dense_layer = min(self.unit_first_dense_layer, backbone.shape[1])
+        # If the dense layer is too much the model will be broken (unit last dense is smaller than output unit)
         max_dense = floor(log2(self.unit_first_dense_layer)) - ceil(log2(self.num_class))
         self.num_dense = min(self.num_dense, max_dense)
 
+        # Create Dense layers
         fc_layer = []
         if self.num_dense > 1:
             fc_layer.append(Dense(units=self.unit_first_dense_layer, activation=self.activation_dense)(backbone))
@@ -78,23 +72,50 @@ class KerasModel:
             if self.dropout_layer:
                 fc_layer.append(Dropout(rate=self.dropout_rate)(fc_layer[-1]))
 
+        # Create output layer
         if self.num_dense == 1:
+            # num_dense == 1 it mean these model don't need any dense layer
             output = Dense(units=self.num_class, activation=self.activation_last_dense)(backbone)
         else:
             output = Dense(units=self.num_class, activation=self.activation_last_dense)(fc_layer[-1])
         return output
 
     def create_model_keras(self) -> tf.keras.Model:
-        input_shape = self._get_base_model_input_shape()
-
-        input_layer = tf.keras.Input(shape=input_shape)
+        input_layer = tf.keras.Input(shape=self.input_shape)
         preprocess_layer = self.preprocess_layer(input_layer)
-        base_model = self.base_model(preprocess_layer)
+        backbone = self.backbone(preprocess_layer)
 
         if self.last_pooling_layer is None:
-            flatten_layer = Flatten()(base_model)
-            output = self._create_fully_connected_layer(backbone=flatten_layer)
+            flatten_layer = Flatten()(backbone)
+            output = self._create_fully_connected_layers(backbone=flatten_layer)
         else:
-            output = self._create_fully_connected_layer(backbone=base_model)
+            output = self._create_fully_connected_layers(backbone=backbone)
 
-        return tf.keras.Model(input_layer, output)
+        self.full_model = tf.keras.Model(input_layer, output)
+
+    def load_checkpoint_weight(self, weights_cp_path=None, weights_cp_dir=None):
+        def load_latest_weight():
+            latest_checkpoint = None
+            for checkpoint in os.listdir(weights_cp_dir):
+                checkpoint = os.path.join(weights_cp_dir, checkpoint)
+                # Format of checkpoint of tensorflow always has 2 file with the prefix:
+                #   *.index
+                #   *.data-xxxxx-of-xxxxx (x is a number)
+                # The xxxxx is change depend on your system so use .index to identify each checkpoint in directory
+                if checkpoint.endswith(".index"):
+                    if latest_checkpoint is None:
+                        latest_checkpoint = checkpoint
+                    elif os.path.getmtime(latest_checkpoint) < os.path.getmtime(checkpoint):
+                        latest_checkpoint = checkpoint
+            return ".".join(latest_checkpoint.split('.')[:-1])
+
+        if weights_cp_path is not None or weights_cp_dir is not None:
+            if weights_cp_dir is not None:
+                weights_cp_path = load_latest_weight()
+            self.full_model.load_weights(weights_cp_path)
+            print("Load checkpoints from ", weights_cp_path)
+        else:
+            warnings.warn("There are no weights checkpoint to load.")
+
+    def get_backbone_weight(self):
+        pass
