8503fb4b275fd51572ddc23202e4502a87ce7967
datn2107 <65413565+datn2107@users.noreply.github.com>
2021-10-12 12:40:06 +0700

    Add comment and doc

--- kerascls/data.py
+++ kerascls/data.py
@@ -5,19 +5,33 @@ from sklearn.model_selection import train_test_split
 
 
 class DataReader:
-    def __init__(self, dataframe, image_dir, batch_size=8, height=None, width=None):
+    def __init__(self, dataframe, image_dir, batch_size=8, height=224, width=224):
+        """ Build tf.data.dataset from dataframe
+
+        Dataframe need contain n+1 column where:
+            Index column is the column contain image name
+            n other columns are the label of image, value is 0 or 1
+        All image WILL be resize to the same size
+        But it WON'T be normalize, it will be handle by preprocessing layer of model\
+
+        :param dataframe: Metadata of dataset you want to build
+        :param image_dir: Path to directory that contain image
+        :param batch_size: Batch size
+        :param height: Image size to resize
+        :param width: Image size to resize
+        """
+
         self.list_image = list(dataframe.index)
-        self.list_label = list(dataframe.apply(list, axis=1).values)
+        self.list_label = list(dataframe.apply(tuple, axis=1).values)
         self.list_image_path = list(map(lambda image: os.path.join(image_dir, image), self.list_image))
         self.batch_size = batch_size
         self.height, self.width = height, width
 
     def _load_image(self, path):
         image = tf.io.read_file(path)
-        image = tf.image.decode_png(image, channels=3)
+        image = tf.image.decode_image(image, channels=3)
         image = tf.cast(image, tf.float32)
-        # resize image depend on what model need
-        # some model does need specific image size
+        # All image in dataset need to be the same size
         if self.height is not None and self.width is not None:
             image = tf.image.resize(image, (self.height, self.width))
         else:
@@ -25,25 +39,31 @@ class DataReader:
 
         return image
 
+    def augment_data(self):
+        pass
+
     def load_dataset(self, training=True):
-        label_tensor = list(map(tf.convert_to_tensor, self.list_label))
-        label_dataset = tf.data.Dataset.from_tensor_slices(label_tensor)
+        label_dataset = tf.data.Dataset.from_tensor_slices(self.list_label)
         image_path_tensor = tf.data.Dataset.from_tensor_slices(self.list_image_path)
         image_dataset = image_path_tensor.map(self._load_image, num_parallel_calls=tf.data.AUTOTUNE)
 
         dataset = tf.data.Dataset.zip((image_dataset, label_dataset))
         if training:
             dataset = dataset.shuffle(1000)
-        dataset = dataset.batch(self.batch_size, drop_remainder=True, num_parallel_calls=tf.data.AUTOTUNE)
+        dataset = dataset.batch(self.batch_size, num_parallel_calls=tf.data.AUTOTUNE)
         dataset = dataset.prefetch(tf.data.AUTOTUNE)
         return dataset
 
 
-def split_and_load_dataset(dataframe, image_dir, batch_size, height, width, train_size=0.7):
+def split_and_load_dataset(dataframe, image_dir, batch_size, height, width, train_size=0.7, val_size=0.15, test_size=0.15):
+    """Split and load data for training and testing"""
+
+    # Split dataframe into 3 part training, validation and testing
     train_dataframe, test_dataframe = train_test_split(dataframe, train_size=train_size, shuffle=True,
                                                        random_state=2107)
-    test_dataframe, val_dataframe = train_test_split(test_dataframe, train_size=0.5)
+    test_dataframe, val_dataframe = train_test_split(test_dataframe, train_size=test_size/(val_size+train_size))
 
+    # Load dataset for each part
     train_dataset = DataReader(train_dataframe, image_dir, batch_size=batch_size, height=height,
                                width=width).load_dataset(training=True)
     val_dataset = DataReader(val_dataframe, image_dir, batch_size, height, width).load_dataset(training=False)
